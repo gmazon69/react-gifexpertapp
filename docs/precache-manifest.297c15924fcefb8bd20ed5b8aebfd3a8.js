7b86e0fb24e792f1e55e141eb1062afc7375c61c
Gregory Mazon <gregorymazon@gmail.com>
2020-11-06 17:18:29 -0600

    docs agregado

---
+++ docs/precache-manifest.297c15924fcefb8bd20ed5b8aebfd3a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df6ac1538687e414b6576fdb7fc5a63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22e6820d3a96c2802534",
+    "url": "/static/css/main.6a540f94.chunk.css"
+  },
+  {
+    "revision": "bd295c093893dc1cc8e9",
+    "url": "/static/js/2.b5aad69a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b5aad69a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e6820d3a96c2802534",
+    "url": "/static/js/main.6b8703d1.chunk.js"
+  },
+  {
+    "revision": "d009f057bd0459dd47a7",
+    "url": "/static/js/runtime-main.a53b5927.js"
+  }
+]);
